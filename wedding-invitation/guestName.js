0a3855748194b10b400cdd6f23e1560a4f18f9a1
hamzbond <hamzbond@gmail.com>
2023-05-30 18:34:54 +0700

    new

--- wedding-invitation/guestName.js
+++ wedding-invitation/guestName.js
@@ -3,9 +3,33 @@ const urlParam = new URLSearchParams(queryString)
 const guestName = urlParam.get('to')
 
 var guestList = [
-    'hamzah',
-    'difa',
-    'Achmad Hamzah'
+    'M. Difa\' Sulthon Diani',
+    'Alfajri Tsaqifurrosyd',
+    'Arif Kurniawan',
+    'Roy Noviantho',
+    'Putri Noviyani',
+    'Icha Inggris Lestari',
+    'Ravika Oktaviani',
+    'Rhony Septian',
+    'Kurniawan Yudha Putrama',
+    'Kern Alexandra Nuroho',
+    'Joseph Clifton Nuroho',
+    'Nurosoft Members',
+    'Shasa Sabrina',
+    'M. Abdillah Wahab',
+    'Abdulloh Baihaki',
+    'Ibadur Rochman',
+    'Fahmia Ayu Lestari',
+    'Ika Zulkafika Mahmudah',
+    'Uswatun Chasanah',
+    'Rizka Amalia Putri',
+    'Maghfiroh Puji Lestari',
+    'Ulifatur Rosyidah',
+    'Ayu Evita Syahrul Fauziyah',
+    'M. Agung Ilham',
+    'Nurul Izzah',
+    'Bintang Robiatul Adawiyah',
+    'Rosa Melyna Mazlin'
 ]
 guestList.forEach(element => {
     if(element.toLowerCase() == guestName.toLowerCase()) {
